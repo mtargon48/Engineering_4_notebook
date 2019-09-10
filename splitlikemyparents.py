76673da79bf0af0609fb1be1fe2ea97c550b6935
mtargon48 <mtargon48@charlottesvilleschools.org>
2019-09-10 12:02:59 -0400

    splitlikemyparents.py

---
+++ splitlikemyparents.py
@@ -0,0 +1,12 @@
+#def split(text):
+text = input("slap a word in there big boi: ")
+
+woogoo = list(text)
+
+for i in woogoo:
+    if(i == " "):
+        print ('-')
+    else:
+        print(i+"\n")
+
+
